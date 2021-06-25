b90595c688d25e596a67e9f74063f92a585f3ca4
Arhangel333 <71559868+Arhangel333@users.noreply.github.com>
2021-06-25 14:11:41 +0300

    Update node.cpp

--- lab6/node.cpp
+++ lab6/node.cpp
@@ -8,22 +8,65 @@ typedef struct MD
 	char message[128];
 } MessageData;
 
-void NodeWork(std::string &str){
+int myId = 0;
+int lport = 0;
 
+void NodeWork(std::string &str, zmq::socket_t &rSocket){
+	std::string cmd;
+	int id, pid;
+	getinfo(str, cmd, id, pid);
+	if(cmd == "ok"){
+		return;
+	}else if(cmd == "create"){
+		if(pid == myId){
+			
+			int childid = fork();
+			if (childid == -1)
+			{ //error
+				perror("Bad fork1\n");
+				exit(0);
+			}
+			if(childid == 0)//for child
+			{
+				printf("PREEXEC\n");
+				std::string cid = std::to_string(id);
+				execl("child", "child", cid.c_str(), NULL);
+			}
+			str = "chcreate " + std::to_string(id) + " " + std::to_string(pid);
+			std::cout<<str<<"<<<<<<<"<<std::endl;
+		}else{
+			//sendM(rSocket, str);
+			printf("Node %d: message sent to next\n", getpid());
+			return;
+		}
+	} else if(cmd == "chcreate"){
+		myId == id? printf("Node %d: true child id\n", getpid()) : printf("Node %d: false child id\n", getpid());
+	}else if(cmd == "end"){
+		sendM(rSocket, "end");
+		printf("Node %d: sent msg to next and finishing\n", getpid());
+		exit(0);
+	}
 }
 
 int main(int argc, char const *argv[])
 {
-	printf("Execute works\n");
-	
+
+	printf("Node %d: Execute works %s\n", getpid(), argv[1]);
+	myId = atoi(argv[1]);
+	lport = atoi(argv[2]);
     zmq::context_t context(1);
-	printf("context done\n");
-    zmq::socket_t nodeSocket(context, ZMQ_REP);
-	printf("Socket done\n");
+	printf("Node %d:  context done\n", getpid());
+    zmq::socket_t lSocket(context, ZMQ_REP);
+	printf("Node %d:  Socket done\n", getpid());
+
+	zmq::socket_t rSocket(context, ZMQ_REQ);
 	
+	//int rport = 0;
+	lport = bindSocket(rSocket, lport);
 	try{
-		std::cout<<"Node connected to "<<connectSocket(nodeSocket)<<std::endl;
-			//nodeSocket.connect(getPortName(8080));
+		lport = connectSocket(lSocket);
+		std::cout<<"Node "<<getpid()<<": Node connected to "<<lport<<std::endl;
+			//lSocket.connect(getlPortName(8080));
 		}catch(...){
 			printf(" %d: %s\n", zmq_errno(), zmq_strerror(zmq_errno()));
 			exit(0);
@@ -31,18 +74,19 @@ int main(int argc, char const *argv[])
 	
     printf("Node id:%d is connected\n", getpid());
 	
-	
+
+	std::string message("ok");
 	
 	
 	for (;;)
 	{
 		//MessageData message;
-		printf("Node: reciving\n");
-		std::string message(receiveMessage(nodeSocket));
-		printf("Message from claster:_%s_\n", message.c_str());
+		printf("Node %d: reciving\n", getpid());
+		message = receiveMessage(lSocket);
+		NodeWork(message, rSocket);
+		printf("Node %d: Message from claster:_%s_\n", getpid(), message.c_str());
 		std::string reply("ok");
-        sendM(nodeSocket, reply);
-		break;
+        sendM(lSocket, reply);
 	}
     printf("Node id:%d ended his work\n", getpid());
 	return 0;
