81b4bb63519a0a0fec5f573badc97244abf435c7
Arhangel333 <71559868+Arhangel333@users.noreply.github.com>
2021-06-25 16:38:31 +0300

    Update node.cpp

--- lab6/node.cpp
+++ lab6/node.cpp
@@ -9,7 +9,8 @@ typedef struct MD
 } MessageData;
 
 int myId = 0;
-int lport = 0;
+int port = 0;
+int havechild = 0;
 
 void NodeWork(std::string &str, zmq::socket_t &rSocket){
 	std::string cmd;
@@ -19,8 +20,11 @@ void NodeWork(std::string &str, zmq::socket_t &rSocket){
 		return;
 	}else if(cmd == "create"){
 		if(pid == myId){
-			
-			int childid = fork();
+			if(havechild == 0){
+			havechild = 1;
+		}
+			int childid = 13;
+			childid = fork();
 			if (childid == -1)
 			{ //error
 				perror("Bad fork1\n");
@@ -28,51 +32,61 @@ void NodeWork(std::string &str, zmq::socket_t &rSocket){
 			}
 			if(childid == 0)//for child
 			{
-				printf("PREEXEC\n");
+				printf("Node %d: PREEXEC %d\n", myId, getpid());
 				std::string cid = std::to_string(id);
-				execl("child", "child", cid.c_str(), NULL);
+				execl("child", "child", cid.c_str(), std::to_string(port).c_str(), NULL);
 			}
-			str = "chcreate " + std::to_string(id) + " " + std::to_string(pid);
-			std::cout<<str<<"<<<<<<<"<<std::endl;
+
+			str = "create " + std::to_string(id) + " " + std::to_string(pid);
+			std::cout<<getpid()<<" is my id after execute"<<"<<<<<<<"<<std::endl;
+		}else if(id != myId){
+			sendM(rSocket, str);
+			printf("Node %d: message sent to next\n", myId);
+			str = receiveMessage(rSocket);
+			printf("Node %d: reply from child: _%s_\n", myId, str.c_str());
 		}else{
-			//sendM(rSocket, str);
-			printf("Node %d: message sent to next\n", getpid());
-			return;
+			printf("I've just bornt\n");
 		}
-	} else if(cmd == "chcreate"){
-		myId == id? printf("Node %d: true child id\n", getpid()) : printf("Node %d: false child id\n", getpid());
+		
+		return;
 	}else if(cmd == "end"){
-		sendM(rSocket, "end");
-		printf("Node %d: sent msg to next and finishing\n", getpid());
+		std::cout<<havechild<<" havech"<<std::endl;
+		 if(havechild == 1){
+			sendM(rSocket, "end");
+			printf("Node %d: sent msg to next and finishing\n", myId);
+		 }else{
+			printf("Node %d: without children finishing\n", myId);
+		 }
 		exit(0);
 	}
 }
 
 int main(int argc, char const *argv[])
 {
-
-	printf("Node %d: Execute works %s\n", getpid(), argv[1]);
 	myId = atoi(argv[1]);
-	lport = atoi(argv[2]);
+	port = atoi(argv[2]);
+	printf("Node %d(%d): Execute works %s\n", myId, getpid(), argv[1]);
+	
     zmq::context_t context(1);
-	printf("Node %d:  context done\n", getpid());
+	printf("Node %d:  context done\n", myId);
     zmq::socket_t lSocket(context, ZMQ_REP);
-	printf("Node %d:  Socket done\n", getpid());
+	
 
 	zmq::socket_t rSocket(context, ZMQ_REQ);
+	printf("Node %d:  Sockets done\n", myId);
+
 	
-	//int rport = 0;
-	lport = bindSocket(rSocket, lport);
 	try{
-		lport = connectSocket(lSocket);
-		std::cout<<"Node "<<getpid()<<": Node connected to "<<lport<<std::endl;
-			//lSocket.connect(getlPortName(8080));
+		port = connectSocket(lSocket, port);
+		std::cout<<"Node "<<myId<<": Node connected to "<<port<<std::endl;
+			port++;
 		}catch(...){
 			printf(" %d: %s\n", zmq_errno(), zmq_strerror(zmq_errno()));
 			exit(0);
 			}
+	port = bindSocket(rSocket, port);
 	
-    printf("Node id:%d is connected\n", getpid());
+    printf("Node id:%d is connected\n", myId);
 	
 
 	std::string message("ok");
@@ -81,13 +95,14 @@ int main(int argc, char const *argv[])
 	for (;;)
 	{
 		//MessageData message;
-		printf("Node %d: reciving\n", getpid());
+		printf("Node %d: reciving\n", myId);
 		message = receiveMessage(lSocket);
+		printf("Node %d: Message from left:_%s_\n", myId, message.c_str());
 		NodeWork(message, rSocket);
-		printf("Node %d: Message from claster:_%s_\n", getpid(), message.c_str());
+		
 		std::string reply("ok");
         sendM(lSocket, reply);
 	}
-    printf("Node id:%d ended his work\n", getpid());
+    printf("Node id:%d ended his work\n", myId);
 	return 0;
 }
